df4d39046c83106235e310bcb4380d419efa3366
sapir hiki <sapirhiki@gmail.com>
2022-03-19 12:43:54 +0200

    Protect the relevant routes using a middleware

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { getUser, getUsers, deleteUser, updateUser } = require('./user.controller')
+const { requireAuth, requireAdmin } = require('../../middlewares/requireAuth.middleware')
 const router = express.Router()
 
 router.get('/', getUsers)
 router.get('/:id', getUser)
 router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
+router.delete('/:id', requireAuth, requireAdmin, deleteUser)
 
 module.exports = router
